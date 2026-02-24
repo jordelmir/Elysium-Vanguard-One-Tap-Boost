5df6a4bdf88dc27490de46cb27befc2b00107653
jordelmir <jordelmir@gmail.com>
2026-02-24 12:13:22 -0600

    Final resource fix and APK generation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.android.material:material:1.12.0")
     
     // Shizuku
     implementation("dev.rikka.shizuku:api:13.1.5")
